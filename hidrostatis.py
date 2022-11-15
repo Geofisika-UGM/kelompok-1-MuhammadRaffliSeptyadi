a8149fe5c44ccd359cc66008d7995445b8e39e25
Zahra Adinda Billqish <billqish97@gmail.com>
2022-11-15 14:51:10 +0700

    Update hidrostatis.py

--- hidrostatis.py
+++ hidrostatis.py
@@ -1,5 +1,14 @@
-def hdr(a,b):
-    """Menghitung Tekanan hidrostatis (massa jenis zat cair,kedalaman) """
-    c=9.8
-    hdr = a*b*c
+def hdr(rho, g, h):
+    """Menghitung Tekanan hidrostatis (massa jenis zat cair,kedalaman) 
+    Argument:
+        rho (float/int) = nilai masa jenis cairannya
+        g (float/int) = nilai gravitasi suatu planet atau bumi
+        h (float/int) = posisi tinggi tangki bocornya
+    Return:
+        hdr = hasil dari perhitungan hidrostatis
+    """
+    rho = float(rho)
+    g = float(g)
+    h = float(h)
+    hdr = rho*g*h
     return hdr
